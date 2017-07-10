258a77290b898c6eb407880b63ef7ac3f952f5ee
Shirley <yanyuxuan0909yu@gmail.com>
2017-07-10 23:29:31 +0800

    Create knnWithData.js

---
+++ knnWithData.js
@@ -0,0 +1,990 @@
+
+let data = [
+    {
+        "class": "2",
+        "valueA": "6.3",
+        "valueB": "2.7",
+        "valueC": "4.9",
+        "valueD": "1.8"
+    },
+    {
+        "class": "1",
+        "valueA": "5.7",
+        "valueB": "2.8",
+        "valueC": "4.5",
+        "valueD": "1.3"
+    },
+    {
+        "class": "1",
+        "valueA": "5.5",
+        "valueB": "2.6",
+        "valueC": "4.4",
+        "valueD": "1.2"
+    },
+    {
+        "class": "0",
+        "valueA": "4.8",
+        "valueB": "3.4",
+        "valueC": "1.6",
+        "valueD": "0.2"
+    },
+    {
+        "class": "2",
+        "valueA": "6.4",
+        "valueB": "2.7",
+        "valueC": "5.3",
+        "valueD": "1.9"
+    },
+    {
+        "class": "2",
+        "valueA": "6.7",
+        "valueB": "3.3",
+        "valueC": "5.7",
+        "valueD": "2.5"
+    },
+    {
+        "class": "0",
+        "valueA": "4.7",
+        "valueB": "3.2",
+        "valueC": "1.6",
+        "valueD": "0.2"
+    },
+    {
+        "class": "1",
+        "valueA": "5.5",
+        "valueB": "2.4",
+        "valueC": "3.7",
+        "valueD": "1"
+    },
+    {
+        "class": "0",
+        "valueA": "4.6",
+        "valueB": "3.2",
+        "valueC": "1.4",
+        "valueD": "0.2"
+    },
+    {
+        "class": "2",
+        "valueA": "6.7",
+        "valueB": "3",
+        "valueC": "5.2",
+        "valueD": "2.3"
+    },
+    {
+        "class": "2",
+        "valueA": "6.4",
+        "valueB": "2.8",
+        "valueC": "5.6",
+        "valueD": "2.2"
+    },
+    {
+        "class": "0",
+        "valueA": "4.8",
+        "valueB": "3.1",
+        "valueC": "1.6",
+        "valueD": "0.2"
+    },
+    {
+        "class": "1",
+        "valueA": "6.7",
+        "valueB": "3.1",
+        "valueC": "4.4",
+        "valueD": "1.4"
+    },
+    {
+        "class": "2",
+        "valueA": "6.3",
+        "valueB": "2.5",
+        "valueC": "5",
+        "valueD": "1.9"
+    },
+    {
+        "class": "1",
+        "valueA": "5.7",
+        "valueB": "3",
+        "valueC": "4.2",
+        "valueD": "1.2"
+    },
+    {
+        "class": "0",
+        "valueA": "5.1",
+        "valueB": "3.5",
+        "valueC": "1.4",
+        "valueD": "0.3"
+    },
+    {
+        "class": "1",
+        "valueA": "5.5",
+        "valueB": "2.3",
+        "valueC": "4",
+        "valueD": "1.3"
+    },
+    {
+        "class": "2",
+        "valueA": "7.3",
+        "valueB": "2.9",
+        "valueC": "6.3",
+        "valueD": "1.8"
+    },
+    {
+        "class": "0",
+        "valueA": "4.8",
+        "valueB": "3",
+        "valueC": "1.4",
+        "valueD": "0.3"
+    },
+    {
+        "class": "2",
+        "valueA": "6.5",
+        "valueB": "3",
+        "valueC": "5.5",
+        "valueD": "1.8"
+    },
+    {
+        "class": "2",
+        "valueA": "6.4",
+        "valueB": "3.1",
+        "valueC": "5.5",
+        "valueD": "1.8"
+    },
+    {
+        "class": "2",
+        "valueA": "7.7",
+        "valueB": "3.8",
+        "valueC": "6.7",
+        "valueD": "2.2"
+    },
+    {
+        "class": "1",
+        "valueA": "6.7",
+        "valueB": "3",
+        "valueC": "5",
+        "valueD": "1.7"
+    },
+    {
+        "class": "1",
+        "valueA": "5.6",
+        "valueB": "3",
+        "valueC": "4.5",
+        "valueD": "1.5"
+    },
+    {
+        "class": "1",
+        "valueA": "5.9",
+        "valueB": "3",
+        "valueC": "4.2",
+        "valueD": "1.5"
+    },
+    {
+        "class": "1",
+        "valueA": "6",
+        "valueB": "3.4",
+        "valueC": "4.5",
+        "valueD": "1.6"
+    },
+    {
+        "class": "2",
+        "valueA": "6.1",
+        "valueB": "3",
+        "valueC": "4.9",
+        "valueD": "1.8"
+    },
+    {
+        "class": "1",
+        "valueA": "5.1",
+        "valueB": "2.5",
+        "valueC": "3",
+        "valueD": "1.1"
+    },
+    {
+        "class": "0",
+        "valueA": "5.1",
+        "valueB": "3.3",
+        "valueC": "1.7",
+        "valueD": "0.5"
+    },
+    {
+        "class": "2",
+        "valueA": "6.4",
+        "valueB": "2.8",
+        "valueC": "5.6",
+        "valueD": "2.1"
+    },
+    {
+        "class": "0",
+        "valueA": "5.7",
+        "valueB": "4.4",
+        "valueC": "1.5",
+        "valueD": "0.4"
+    },
+    {
+        "class": "1",
+        "valueA": "5.8",
+        "valueB": "2.6",
+        "valueC": "4",
+        "valueD": "1.2"
+    },
+    {
+        "class": "1",
+        "valueA": "5.5",
+        "valueB": "2.5",
+        "valueC": "4",
+        "valueD": "1.3"
+    },
+    {
+        "class": "2",
+        "valueA": "6.8",
+        "valueB": "3.2",
+        "valueC": "5.9",
+        "valueD": "2.3"
+    },
+    {
+        "class": "1",
+        "valueA": "6",
+        "valueB": "2.2",
+        "valueC": "4",
+        "valueD": "1"
+    },
+    {
+        "class": "2",
+        "valueA": "6.2",
+        "valueB": "3.4",
+        "valueC": "5.4",
+        "valueD": "2.3"
+    },
+    {
+        "class": "0",
+        "valueA": "5.2",
+        "valueB": "4.1",
+        "valueC": "1.5",
+        "valueD": "0.1"
+    },
+    {
+        "class": "2",
+        "valueA": "7.7",
+        "valueB": "3",
+        "valueC": "6.1",
+        "valueD": "2.3"
+    },
+    {
+        "class": "2",
+        "valueA": "6.5",
+        "valueB": "3.2",
+        "valueC": "5.1",
+        "valueD": "2"
+    },
+    {
+        "class": "0",
+        "valueA": "5.4",
+        "valueB": "3.4",
+        "valueC": "1.7",
+        "valueD": "0.2"
+    },
+    {
+        "class": "2",
+        "valueA": "6.5",
+        "valueB": "3",
+        "valueC": "5.2",
+        "valueD": "2"
+    },
+    {
+        "class": "1",
+        "valueA": "6.3",
+        "valueB": "3.3",
+        "valueC": "4.7",
+        "valueD": "1.6"
+    },
+    {
+        "class": "0",
+        "valueA": "5",
+        "valueB": "3.3",
+        "valueC": "1.4",
+        "valueD": "0.2"
+    },
+    {
+        "class": "2",
+        "valueA": "6.9",
+        "valueB": "3.1",
+        "valueC": "5.4",
+        "valueD": "2.1"
+    },
+    {
+        "class": "1",
+        "valueA": "5.6",
+        "valueB": "2.9",
+        "valueC": "3.6",
+        "valueD": "1.3"
+    },
+    {
+        "class": "0",
+        "valueA": "5.2",
+        "valueB": "3.4",
+        "valueC": "1.4",
+        "valueD": "0.2"
+    },
+    {
+        "class": "0",
+        "valueA": "4.7",
+        "valueB": "3.2",
+        "valueC": "1.3",
+        "valueD": "0.2"
+    },
+    {
+        "class": "1",
+        "valueA": "6.6",
+        "valueB": "2.9",
+        "valueC": "4.6",
+        "valueD": "1.3"
+    },
+    {
+        "class": "0",
+        "valueA": "4.4",
+        "valueB": "3.2",
+        "valueC": "1.3",
+        "valueD": "0.2"
+    },
+    {
+        "class": "2",
+        "valueA": "6.8",
+        "valueB": "3",
+        "valueC": "5.5",
+        "valueD": "2.1"
+    },
+    {
+        "class": "1",
+        "valueA": "5.8",
+        "valueB": "2.7",
+        "valueC": "4.1",
+        "valueD": "1"
+    },
+    {
+        "class": "0",
+        "valueA": "4.8",
+        "valueB": "3.4",
+        "valueC": "1.9",
+        "valueD": "0.2"
+    },
+    {
+        "class": "0",
+        "valueA": "5.7",
+        "valueB": "3.8",
+        "valueC": "1.7",
+        "valueD": "0.3"
+    },
+    {
+        "class": "0",
+        "valueA": "4.4",
+        "valueB": "2.9",
+        "valueC": "1.4",
+        "valueD": "0.2"
+    },
+    {
+        "class": "2",
+        "valueA": "7.2",
+        "valueB": "3",
+        "valueC": "5.8",
+        "valueD": "1.6"
+    },
+    {
+        "class": "0",
+        "valueA": "5.1",
+        "valueB": "3.8",
+        "valueC": "1.6",
+        "valueD": "0.2"
+    },
+    {
+        "class": "0",
+        "valueA": "5",
+        "valueB": "3.2",
+        "valueC": "1.2",
+        "valueD": "0.2"
+    },
+    {
+        "class": "0",
+        "valueA": "5.1",
+        "valueB": "3.4",
+        "valueC": "1.5",
+        "valueD": "0.2"
+    },
+    {
+        "class": "2",
+        "valueA": "6",
+        "valueB": "2.2",
+        "valueC": "5",
+        "valueD": "1.5"
+    },
+    {
+        "class": "1",
+        "valueA": "5.6",
+        "valueB": "3",
+        "valueC": "4.1",
+        "valueD": "1.3"
+    },
+    {
+        "class": "1",
+        "valueA": "5.9",
+        "valueB": "3.2",
+        "valueC": "4.8",
+        "valueD": "1.8"
+    },
+    {
+        "class": "0",
+        "valueA": "4.6",
+        "valueB": "3.6",
+        "valueC": "1",
+        "valueD": "0.2"
+    },
+    {
+        "class": "1",
+        "valueA": "5",
+        "valueB": "2.3",
+        "valueC": "3.3",
+        "valueD": "1"
+    },
+    {
+        "class": "0",
+        "valueA": "5.1",
+        "valueB": "3.8",
+        "valueC": "1.5",
+        "valueD": "0.3"
+    },
+    {
+        "class": "1",
+        "valueA": "6.4",
+        "valueB": "2.9",
+        "valueC": "4.3",
+        "valueD": "1.3"
+    },
+    {
+        "class": "2",
+        "valueA": "6",
+        "valueB": "3",
+        "valueC": "4.8",
+        "valueD": "1.8"
+    },
+    {
+        "class": "1",
+        "valueA": "6.6",
+        "valueB": "3",
+        "valueC": "4.4",
+        "valueD": "1.4"
+    },
+    {
+        "class": "2",
+        "valueA": "5.8",
+        "valueB": "2.7",
+        "valueC": "5.1",
+        "valueD": "1.9"
+    },
+    {
+        "class": "1",
+        "valueA": "6.1",
+        "valueB": "2.8",
+        "valueC": "4.7",
+        "valueD": "1.2"
+    },
+    {
+        "class": "0",
+        "valueA": "4.9",
+        "valueB": "3.1",
+        "valueC": "1.5",
+        "valueD": "0.1"
+    },
+    {
+        "class": "1",
+        "valueA": "6.1",
+        "valueB": "3",
+        "valueC": "4.6",
+        "valueD": "1.4"
+    },
+    {
+        "class": "2",
+        "valueA": "6.3",
+        "valueB": "2.8",
+        "valueC": "5.1",
+        "valueD": "1.5"
+    },
+    {
+        "class": "1",
+        "valueA": "5.7",
+        "valueB": "2.6",
+        "valueC": "3.5",
+        "valueD": "1"
+    },
+    {
+        "class": "0",
+        "valueA": "4.3",
+        "valueB": "3",
+        "valueC": "1.1",
+        "valueD": "0.1"
+    },
+    {
+        "class": "1",
+        "valueA": "6.5",
+        "valueB": "2.8",
+        "valueC": "4.6",
+        "valueD": "1.5"
+    },
+    {
+        "class": "1",
+        "valueA": "5.7",
+        "valueB": "2.8",
+        "valueC": "4.1",
+        "valueD": "1.3"
+    },
+    {
+        "class": "1",
+        "valueA": "6.3",
+        "valueB": "2.5",
+        "valueC": "4.9",
+        "valueD": "1.5"
+    },
+    {
+        "class": "0",
+        "valueA": "5.5",
+        "valueB": "4.2",
+        "valueC": "1.4",
+        "valueD": "0.2"
+    },
+    {
+        "class": "1",
+        "valueA": "6.7",
+        "valueB": "3.1",
+        "valueC": "4.7",
+        "valueD": "1.5"
+    },
+    {
+        "class": "1",
+        "valueA": "6.8",
+        "valueB": "2.8",
+        "valueC": "4.8",
+        "valueD": "1.4"
+    },
+    {
+        "class": "0",
+        "valueA": "4.6",
+        "valueB": "3.4",
+        "valueC": "1.4",
+        "valueD": "0.3"
+    },
+    {
+        "class": "2",
+        "valueA": "5.6",
+        "valueB": "2.8",
+        "valueC": "4.9",
+        "valueD": "2"
+    },
+    {
+        "class": "2",
+        "valueA": "7.7",
+        "valueB": "2.8",
+        "valueC": "6.7",
+        "valueD": "2"
+    },
+    {
+        "class": "2",
+        "valueA": "6.3",
+        "valueB": "3.3",
+        "valueC": "6",
+        "valueD": "2.5"
+    },
+    {
+        "class": "1",
+        "valueA": "6.3",
+        "valueB": "2.3",
+        "valueC": "4.4",
+        "valueD": "1.3"
+    },
+    {
+        "class": "1",
+        "valueA": "6",
+        "valueB": "2.9",
+        "valueC": "4.5",
+        "valueD": "1.5"
+    },
+    {
+        "class": "0",
+        "valueA": "4.5",
+        "valueB": "2.3",
+        "valueC": "1.3",
+        "valueD": "0.3"
+    },
+    {
+        "class": "2",
+        "valueA": "5.8",
+        "valueB": "2.8",
+        "valueC": "5.1",
+        "valueD": "2.4"
+    },
+    {
+        "class": "2",
+        "valueA": "6.3",
+        "valueB": "2.9",
+        "valueC": "5.6",
+        "valueD": "1.8"
+    },
+    {
+        "class": "0",
+        "valueA": "4.9",
+        "valueB": "3.1",
+        "valueC": "1.5",
+        "valueD": "0.1"
+    },
+    {
+        "class": "1",
+        "valueA": "5.7",
+        "valueB": "2.9",
+        "valueC": "4.2",
+        "valueD": "1.3"
+    },
+    {
+        "class": "0",
+        "valueA": "5.1",
+        "valueB": "3.5",
+        "valueC": "1.4",
+        "valueD": "0.2"
+    },
+    {
+        "class": "0",
+        "valueA": "5.4",
+        "valueB": "3.9",
+        "valueC": "1.7",
+        "valueD": "0.4"
+    },
+    {
+        "class": "0",
+        "valueA": "5.4",
+        "valueB": "3.7",
+        "valueC": "1.5",
+        "valueD": "0.2"
+    },
+    {
+        "class": "2",
+        "valueA": "7.6",
+        "valueB": "3",
+        "valueC": "6.6",
+        "valueD": "2.1"
+    },
+    {
+        "class": "1",
+        "valueA": "5.4",
+        "valueB": "3",
+        "valueC": "4.5",
+        "valueD": "1.5"
+    },
+    {
+        "class": "1",
+        "valueA": "6.2",
+        "valueB": "2.9",
+        "valueC": "4.3",
+        "valueD": "1.3"
+    },
+    {
+        "class": "2",
+        "valueA": "6.9",
+        "valueB": "3.1",
+        "valueC": "5.1",
+        "valueD": "2.3"
+    },
+    {
+        "class": "2",
+        "valueA": "5.8",
+        "valueB": "2.7",
+        "valueC": "5.1",
+        "valueD": "1.9"
+    }];
+let dataToClassify = [
+    {
+        "class": "未知",
+        "valueA": "7.2",
+        "valueB": "3.2",
+        "valueC": "6",
+        "valueD": "1.8"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.4",
+        "valueB": "3.4",
+        "valueC": "1.5",
+        "valueD": "0.4"
+    },
+    {
+        "class": "未知",
+        "valueA": "6.2",
+        "valueB": "2.8",
+        "valueC": "4.8",
+        "valueD": "1.8"
+    },
+    {
+        "class": "未知",
+        "valueA": "5",
+        "valueB": "3.4",
+        "valueC": "1.6",
+        "valueD": "0.4"
+    },
+    {
+        "class": "未知",
+        "valueA": "5",
+        "valueB": "3.5",
+        "valueC": "1.3",
+        "valueD": "0.3"
+    },
+    {
+        "class": "未知",
+        "valueA": "4.9",
+        "valueB": "2.5",
+        "valueC": "4.5",
+        "valueD": "1.7"
+    },
+    {
+        "class": "未知",
+        "valueA": "6.1",
+        "valueB": "2.6",
+        "valueC": "5.6",
+        "valueD": "1.4"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.8",
+        "valueB": "2.7",
+        "valueC": "3.9",
+        "valueD": "1.2"
+    },
+    {
+        "class": "未知",
+        "valueA": "7",
+        "valueB": "3.2",
+        "valueC": "4.7",
+        "valueD": "1.4"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.4",
+        "valueB": "3.9",
+        "valueC": "1.3",
+        "valueD": "0.4"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.9",
+        "valueB": "3",
+        "valueC": "5.1",
+        "valueD": "1.8"
+    },
+    {
+        "class": "未知",
+        "valueA": "5",
+        "valueB": "3.6",
+        "valueC": "1.4",
+        "valueD": "0.2"
+    },
+    {
+        "class": "未知",
+        "valueA": "4.9",
+        "valueB": "3.1",
+        "valueC": "1.5",
+        "valueD": "0.1"
+    },
+    {
+        "class": "未知",
+        "valueA": "6.9",
+        "valueB": "3.1",
+        "valueC": "4.9",
+        "valueD": "1.5"
+    },
+    {
+        "class": "未知",
+        "valueA": "6.9",
+        "valueB": "3.2",
+        "valueC": "5.7",
+        "valueD": "2.3"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.5",
+        "valueB": "2.4",
+        "valueC": "3.8",
+        "valueD": "1.1"
+    },
+    {
+        "class": "未知",
+        "valueA": "6.4",
+        "valueB": "3.2",
+        "valueC": "5.3",
+        "valueD": "2.3"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.2",
+        "valueB": "3.5",
+        "valueC": "1.5",
+        "valueD": "0.2"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.7",
+        "valueB": "2.5",
+        "valueC": "5",
+        "valueD": "2"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.8",
+        "valueB": "4",
+        "valueC": "1.2",
+        "valueD": "0.2"
+    },
+    {
+        "class": "未知",
+        "valueA": "7.7",
+        "valueB": "2.6",
+        "valueC": "6.9",
+        "valueD": "2.3"
+    },
+    {
+        "class": "未知",
+        "valueA": "7.9",
+        "valueB": "3.8",
+        "valueC": "6.4",
+        "valueD": "2"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.2",
+        "valueB": "2.7",
+        "valueC": "3.9",
+        "valueD": "1.4"
+    },
+    {
+        "class": "未知",
+        "valueA": "4.6",
+        "valueB": "3.1",
+        "valueC": "1.5",
+        "valueD": "0.2"
+    },
+    {
+        "class": "未知",
+        "valueA": "6.7",
+        "valueB": "3.3",
+        "valueC": "5.7",
+        "valueD": "2.1"
+    },
+    {
+        "class": "未知",
+        "valueA": "5",
+        "valueB": "2",
+        "valueC": "3.5",
+        "valueD": "1"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.6",
+        "valueB": "2.7",
+        "valueC": "4.2",
+        "valueD": "1.3"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.6",
+        "valueB": "2.5",
+        "valueC": "3.9",
+        "valueD": "1.1"
+    },
+    {
+        "class": "未知",
+        "valueA": "6.1",
+        "valueB": "2.9",
+        "valueC": "4.7",
+        "valueD": "1.4"
+    },
+    {
+        "class": "未知",
+        "valueA": "7.2",
+        "valueB": "3.6",
+        "valueC": "6.1",
+        "valueD": "2.5"
+    },
+    {
+        "class": "未知",
+        "valueA": "6.3",
+        "valueB": "3.4",
+        "valueC": "5.6",
+        "valueD": "2.4"
+    },
+    {
+        "class": "未知",
+        "valueA": "5.1",
+        "valueB": "3.7",
+        "valueC": "1.5",
+        "valueD": "0.4"
+    },
+    {
+        "class": "未知",
+        "valueA": "4.9",
+        "valueB": "2.4",
+        "valueC": "3.3",
+        "valueD": "1"
+    },
+    {
+        "class": "未知",
+        "valueA": "7.4",
+        "valueB": "2.8",
+        "valueC": "6.1",
+        "valueD": "1.9"
+    }
+];
+
+let classes = { 'class0':[], 'class1':[], 'class2':[]};
+let K = 10;
+
+let length = data.length;
+
+start();
+
+function start(){
+    let ld = dataToClassify.length;
+    for (let i = 0; i < ld; ++i) {
+        classify(dataToClassify[i]);
+    }
+}
+
+function classify(a) {
+    for (let i = 0; i < length; ++i ) {
+        let l = O(a, data[i]);
+        classes['class' + data[i].class].push(l);
+    }
+    classes.class0.sort();
+    classes.class1.sort();
+    classes.class2.sort();
+
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    for (let p = 0; p < K; ++p) {
+        let c0 = classes.class0[i];
+        let c1 = classes.class1[j];
+        let c2 = classes.class2[k];
+
+        if (c0 <= c1 && c0 <= c2) {
+            ++ i;
+        } else if (c1 <= c0 && c1 <= c2) {
+            ++ j;
+        } else {
+            ++ k;
+        }
+    }
+
+    if ( i >= j && i >= k) {
+        a.class = '0';
+    } else if ( j >=i && j>=k) {
+        a.class = '1'
+    } else {
+        a.class = '2';
+    }
+}
+
+function O(a, b){
+    return Math.pow((a.valueA - b.valueA), 2) + Math.pow((a.valueB - b.valueB), 2)
+        + Math.pow((a.valueC - b.valueC), 2) + Math.pow((a.valueD - b.valueD), 2);
+}
